0effe47a3a26c50501ab7f57ec0711e51ee9fc1b
ganaha-mikoto <mosimosimikotodesu@icloud.com>
2024-09-29 20:04:17 +0900

    index.html.erbの修正
    ユーザー登録のリンクまで

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources requests:, only: [:index, :new, :create]
+  resources :requests, only: [:index, :new, :create]
   devise_for :users
   root to: "requests#index"
 end
