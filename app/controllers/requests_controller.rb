4185b9f8cc1c62be1fd572509a9aaa7275ccb682
ganaha-mikoto <mosimosimikotodesu@icloud.com>
2024-09-29 18:28:15 +0900

    コントローラーの編集
    index/new/createアクションを定義

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,10 +1,30 @@
 class RequestsController < ApplicationController
+  # 全リクエストの一覧を表示
   def index
+    @requests = Request.all
   end
 
+  # 新規リクエスト投稿フォームを表示
   def new
+    @request = Request.new
   end
 
+  # 新規リクエストを作成
   def create
+    @request = Request.new(request_params)
+    @request.user = current_user # ログインユーザーをリクエストのユーザーに設定
+
+    if @request.save
+      redirect_to requests_path, notice: 'リクエストが作成されました。'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  # Strong Parametersで許可されたパラメータを定義
+  def request_params
+    params.require(:request).permit(:title, :description, :category_id, :max_price, :min_price, :image, :shipping_charge_id)
   end
 end
