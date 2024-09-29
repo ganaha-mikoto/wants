9f5b93b91c85380f4ace4313d6e51e0f781f6c2b
ganaha-mikoto <mosimosimikotodesu@icloud.com>
2024-09-29 18:02:38 +0900

    コントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'requests/index'
+  get 'requests/new'
+  get 'requests/create'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "requests#index"
 end
