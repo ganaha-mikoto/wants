15f4526ffd03f567641f479586fafe2423ad78a9
ganaha-mikoto <mosimosimikotodesu@icloud.com>
2024-09-29 18:04:26 +0900

    ルーティングの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'requests/index'
-  get 'requests/new'
-  get 'requests/create'
+  resources requests:, only: [:index, :new, :create]
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root to: "requests#index"
 end
