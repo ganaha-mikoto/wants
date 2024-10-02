9e7602b7eeca6e2b0993fcf76f3ca839d921c773
ganaha-mikoto <mosimosimikotodesu@icloud.com>
2024-10-02 23:25:35 +0900

    ストロングパラメータのセットと呼び出し

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-
+  before_action :configure_permitted_parameters, if: :devise_controller?  # 追加
 
   private
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
+  end
+
+  # 追加済みの configure_permitted_parameters メソッド
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(
+      :sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday, :email]
+    )
   end
 end
